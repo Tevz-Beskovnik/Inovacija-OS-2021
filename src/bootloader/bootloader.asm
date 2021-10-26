132f73cfbc2bdae9f9cab5e85e19debe2733b413
Tevž <tevz.beskovnik@gmail.com>
2021-10-26 15:54:52 +0200

    added more comments

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -2,14 +2,14 @@
 
 global _start
 _start: 
-    mov [DISK_NUM], dl
+    mov [DISK_NUM], dl ; remember that the current drive is stored in dl save it for later
 
-    mov si, boot_welcome
+    mov si, boot_welcome ; print the welcome message
     call print
 
-    call load_extended
+    call load_extended ; read the kernal from disk
 
-    call enter_protected_mode 
+    call enter_protected_mode ; enter protected mode to work in x86
 
     jmp $
 
@@ -23,9 +23,9 @@ load_extended:
     call read_disk
     ret
 
-KERNAL_OFFSET equ 0x7e00
+KERNAL_OFFSET equ 0x7e00 ; offset at witch the kernel is located at
 
-DISK_NUM equ 0
+DISK_NUM equ 0 ; number of the disk
 
-times 510 - ($-$$) db 0
-dw 0xaa55
+times 510 - ($-$$) db 0 ; fill the rest of the bootsector till its 512 bytes
+dw 0xaa55 ; magic boot number ;)
--- src/bootloader/protectedMode.asm
+++ src/bootloader/protectedMode.asm
@@ -4,21 +4,21 @@
 %include "A20_line.asm"
 
 enter_protected_mode:
-    call enable_A20
+    call enable_A20 ; enable the A20 line (32 bit address line)
     cli 
-    lgdt [gdt_descriptor]
-    mov eax, cr0
+    lgdt [gdt_descriptor] ; enable gdt
+    mov eax, cr0 
     or eax, 1
-    mov cr0, eax
-    jmp gdt_code_seg:start_protected_mode
+    mov cr0, eax ; load bit of cr0 so it knows its in protected mode
+    jmp gdt_code_seg:start_protected_mode ; enter protected mode code located in gdt
 
 [bits 32]
 start_protected_mode:
-    mov ax, gdt_data_seg ; far jump
+    mov ax, gdt_data_seg ; far jump / flush
     mov ds, ax
     mov ss, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
 
-    jmp gdt_code_seg:0x7e00
+    jmp gdt_code_seg:0x7e00 ; jump to where the kernel is located at in memory in gdt
